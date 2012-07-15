13eab2cde143805bb7be9207c16f116baf8d615c
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2012-07-15 21:52:11 +0200

    new test setup

--- runshell.py
+++ runshell.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
-from djnetaxept.test_utils.cli import configure
-import argparse
-import os
+from djeasytests.testsetup import TestSetup, default_settings
 
-def main(wsdl, merchantid, token):
-    
-    configure(
-        DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': 'djnetaxept.sqlite',
-            }
-        },
-        NETAXEPT_WSDL = wsdl,
-        NETAXEPT_MERCHANTID = merchantid,
-        NETAXEPT_TOKEN = token
-    )
-    new_db = not os.path.exists('djnetaxept.sqlite')
-    from django.core.management import call_command
-    call_command('syncdb', interactive=False, migrate_all=new_db)
-    call_command('migrate', interactive=False, fake=new_db)
-    call_command('shell')
+class NetaxeptTestSetup(TestSetup):
+        
+    def get_argparser(self):
+        parser = super(NetaxeptTestSetup, self).get_argparser()
+        parser.add_argument('--wsdl', action='store', dest='wsdl', default='https://epayment-test.bbs.no/netaxept.svc?wsdl')
+        parser.add_argument('--merchantid', action='store', dest='merchantid', required=True)
+        parser.add_argument('--token', action='store', dest='token', required=True)
+        return parser
+        
+    def handle_args(self, args):
+        return dict(
+            NETAXEPT_WSDL = args.wsdl,
+            NETAXEPT_MERCHANTID = args.merchantid,
+            NETAXEPT_TOKEN = args.token
+        )
+
+default_settings.update(dict(
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'djnetaxept.sqlite',
+        }
+    }
+))
+
+testsetup = NetaxeptTestSetup(appname='djnetaxept', default_settings=default_settings)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--wsdl', action='store', dest='wsdl', default='https://epayment-test.bbs.no/netaxept.svc?wsdl')
-    parser.add_argument('--merchantid', action='store', dest='merchantid', required=True)
-    parser.add_argument('--token', action='store', dest='token', required=True)
-    args = parser.parse_args()
-    main(args.wsdl, args.merchantid, args.token)
+    testsetup.run('shell')
